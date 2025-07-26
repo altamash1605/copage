5a4d17f3cf95487f4c170f2ed5a39d9b3d68ef9d
Altamash <altamash16.05@gmail.com>
2025-07-26 18:37:51 +0530

    Make mobile friendly changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,19 @@ import logo from '../assets/logo.svg';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-20 md:px-10 py-4 max-w-7xl mx-auto">
+    <section>
+    <nav className="hidden md:flex flex-row shrink-0 justify-between items-center px-20 md:px-10 py-4 max-w-7xl mx-auto">
       <img src={logo} alt="CoPage logo" className="md:h-15 w-auto" />
-      <ul className="md:flex gap-8 text-lg font-light text-gray-800">
+      <ul className="flex flex-row gap-8 text-lg font-light text-gray-800">
         <li><a href="#home" className="hover:text-brand">Home</a></li>
         <li><a href="#about" className="hover:text-brand">About</a></li>
         <li><a href="#portfolio" className="hover:text-brand">Portfolio</a></li>
         <li><a href="#build" className="hover:text-brand">Let’s Build</a></li>
       </ul>
     </nav>
-    
+    <nav className="md:hidden flex flex-col justify-between items-center px-20 md:px-10 py-4 max-w-7xl mx-auto">
+      <img src={logo} alt="CoPage logo" className="h-20 w-auto" />
+    </nav>
+    </section>
   );
 }
