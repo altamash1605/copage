9edd5e2131ac138a5c6403b79ca6333529c2cab6
Altamash <altamash16.05@gmail.com>
2025-07-26 20:06:31 +0530

    Added Dropdown Menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ import ProfileCard from './ProfileCard';
 
 export default function Hero() {
   return (
-    <section className="max-w-7xl mx-auto md:px-10 px-10 py-10 flex flex-col md:flex-row items-center justify-between md:gap-12 gap-40">
+    <section className="max-w-7xl mx-auto md:px-10 px-10 py-10 flex flex-col md:flex-row md:items-center items-start justify-between md:gap-12 gap-40">
  
     
       {/* Left Content */}
-      <div className="md:max-w-xl mt-28 md:mt-10">
-    <h1 className="text-[50px] md:text-[55px] font-extrabold leading-tight font-poppins">
+      <div className="md:max-w-xl mt-52 md:mt-10">
+    <h1 className="text-5xl md:text-7xl font-extrabold leading-tight font-poppins">
           Designing The Web.<br />
           <span className="text-brand">Together.</span>
         </h1>
@@ -23,7 +23,7 @@ export default function Hero() {
       </div>
       {/* Container with profile + arrow + name */}
       
-      <div className="relative flex justify-center">
+      <div className="hidden relative md:flex justify-center">
         <ProfileCard />
 
         {/* Arrow SVG overlay */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import logo from '../assets/logo.svg';
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <section>
     <nav className="hidden md:flex flex-row shrink-0 justify-between items-center px-20 md:px-10 py-4 max-w-7xl mx-auto">
@@ -12,8 +15,19 @@ export default function Navbar() {
         <li><a href="#build" className="hover:text-brand">Let’s Build</a></li>
       </ul>
     </nav>
-    <nav className="md:hidden flex flex-col justify-between items-center px-20 md:px-10 py-4 max-w-7xl mx-auto">
+    <nav className="md:hidden relative flex flex-col justify-between items-center px-10 mx-auto w-64">
+      <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden">  
       <img src={logo} alt="CoPage logo" className="h-20 w-auto" />
+      </button>
+      {/*Dropdown for mobile*/}
+      {menuOpen && (
+        <ul className="md:hidden absolute top-12 left-[6.3rem] leading-[0.8] mt-6 space-y-4 text-5xl font-bold font-poppins text-[#1F2937] text-opacity-25">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#story">Story</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#build">Build</a></li>
+        </ul>
+      )}
     </nav>
     </section>
   );
