50bfeb50f02939d57f8a93871f1b1b51d6a111ea
Altamash <altamash16.05@gmail.com>
2025-07-28 17:18:26 +0530

    Added Story.jsx and fixed fa-icons error

--- src/components/Build.jsx
+++ src/components/Build.jsx
@@ -1,6 +1,6 @@
 import Form from './Form';
 import Testimonials from './Testimonials';
-import { FaInstagram, FaWhatsapp, FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram, FaWhatsapp, FaTwitter } from 'react-icons/fa';
 
 export default function Build() {
   return (
@@ -33,7 +33,7 @@ export default function Build() {
                 <FaWhatsapp className="hover:text-brand transition" />
               </a>
               <a href="https://x.com" target="_blank" rel="noopener noreferrer">
-                <FaXTwitter className="hover:text-brand transition" />
+                <FaTwitter className="hover:text-brand transition" />
               </a>
             </div>
           </div>
