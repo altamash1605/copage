4eaf82054f234417ac7c7ca06dbeed2a0aa3bd68
Altamash <49805368+altamash1605@users.noreply.github.com>
2025-07-29 21:27:26 +0530

    Update App.jsx to add CookieConsent

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Projects from './components/Projects';
 import Build from './components/Build';
 import Story from './components/Story';
 import Footer from './components/Footer';
+import CookieConsent from './components/CookieConsent';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Build />
       </main>
       <Footer />
+      <CookieConsent />
     </>
   );
 }
