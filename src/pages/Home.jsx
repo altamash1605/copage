d64c94b2e0e402630dbad816c45066fe16a5fcd9
Altamash <49805368+altamash1605@users.noreply.github.com>
2025-07-30 22:21:05 +0530

    Update Home.jsx, migrate App.jsx to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,29 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Build from './components/Build';
+import Story from './components/Story';
+import Footer from './components/Footer';
+import CookieConsent from './components/CookieConsent';
+import NamePrompt from './components/NamePrompt';
 
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Story />
+        <Projects />
+        <Build />
+      </main>
+      <Footer />
+      <CookieConsent />
+      <NamePrompt />
+    </>
+  );
+}
+
+export default App;
