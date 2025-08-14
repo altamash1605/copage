3ccac4c358f1412ee6950f95a1df7b5da64d2fa5
Altamash <49805368+altamash1605@users.noreply.github.com>
2025-08-15 04:49:43 +0530

    Update App.jsx to add login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProjectDetail from './pages/ProjectDetail';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/:slug" element={<ProjectDetail />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
