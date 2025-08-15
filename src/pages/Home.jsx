4358ad049221643ff317f179cabb67933e04890e
Altamash <49805368+altamash1605@users.noreply.github.com>
2025-08-15 05:38:32 +0530

    Update Home.jsx to add WelcomeModal.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../client';
+
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
@@ -5,15 +8,37 @@ import Build from '../components/Build';
 import Story from '../components/Story';
 import Footer from '../components/Footer';
 import CookieConsent from '../components/CookieConsent';
+import WelcomeModal from '../components/WelcomeModal';
 import NamePrompt from '../components/NamePrompt';
 
+function Home() {
+  const [user, setUser] = useState(null);
+  const [showWelcome, setShowWelcome] = useState(false);
 
+  useEffect(() => {
+    async function fetchUser() {
+      const { data: { user } } = await supabase.auth.getUser();
 
-function App() {
+      if (user) {
+        setUser(user);
+        const seenWelcome = sessionStorage.getItem('seenWelcome');
+        if (!seenWelcome) {
+          setShowWelcome(true);
+          sessionStorage.setItem('seenWelcome', 'true');
+        }
+      }
+    }
+
+    fetchUser();
+  }, []);
+
+  const firstName = user?.user_metadata?.full_name?.split(' ')[0] || 'there';
+
   return (
     <>
       <Navbar />
       <main>
+        {showWelcome && <WelcomeModal name={firstName} onClose={() => setShowWelcome(false)} />}
         <Hero />
         <Story />
         <Projects />
@@ -21,9 +46,9 @@ function App() {
       </main>
       <Footer />
       <CookieConsent />
-      <NamePrompt />
+      {!user && <NamePrompt />}
     </>
   );
 }
 
-export default App;
+export default Home;
